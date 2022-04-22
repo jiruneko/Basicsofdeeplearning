880bf0592c7909312247aef4ccc507699859fd0c
jiruneko <oohikaru310@gmail.com>
2022-04-22 09:41:33 +0900

    first commit

---
+++ array.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+X = np.array([1.0,0.5])
+W = np.array([[0.1,0.3,0.5], [0.2,0.4,0.6]])
+B = np.array([0.1,0.2,0.3])
+print(W.shape)
+print(X.shape)
+print(B.shape)
---
+++ softmax.py
@@ -0,0 +1,5 @@
+import numpy as np
+a = np.array([0.3,2.9,4.0])
+
+exp_a = np.exp(a)
+print(exp_a)
